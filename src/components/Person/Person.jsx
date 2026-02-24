d7662fc24c7b5a3a6edc639103928ca67dddb78e
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-24 11:13:48 +0200

    add valiable above the return for person partner

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,23 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age !== undefined ? (
-      <p className="Person__age">{`I am ${person.age}`}</p>
-    ) : null}
-    {person.isMarried && person.sex === 'm' ? (
-      <p className="Person__partner">{`${person.partnerName} is my wife`}</p>
-    ) : null}
-    {person.isMarried && person.sex === 'f' ? (
-      <p className="Person__partner">{`${person.partnerName} is my husband`}</p>
-    ) : null}
-    {!person.isMarried ? (
-      <p className="Person__partner">I am not married</p>
-    ) : null}
-  </section>
-);
+export const Person = ({ person }) => {
+  let partnerNode = '';
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      partnerNode = `${person.partnerName} is my wife`;
+    } else {
+      partnerNode = `${person.partnerName} is my husband`;
+    }
+  } else {
+    partnerNode = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age !== undefined ? (
+        <p className="Person__age">{`I am ${person.age}`}</p>
+      ) : null}
+      <p className="Person__partner">{partnerNode}</p>
+    </section>
+  );
+};
